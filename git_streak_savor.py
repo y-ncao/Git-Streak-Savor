783574a5434684b16796c8a7967933303d8eb5f8
Yan Cao <cyandterry@hotmail.com>
2014-07-24 19:05:05 -0400

    Using crontab now. but app is not add/commit/push

--- git_streak_savor.py
+++ git_streak_savor.py
@@ -3,6 +3,7 @@
 import argparse
 import re
 import os
+import logging
 from sh import git
 from datetime import datetime, timedelta
 
@@ -17,19 +18,29 @@ path_list = ['/Documents/Python_Study',
              '/Documents/jekyll-blog',]
 target_file = '/Documents/jekyll-blog/_posts/2014-07-12-github-streak-savor.md'
 target_path = HOME_DIR + target_file
+
+log_path = '/home/ycao/Dropbox/src/git_streak_savor.log'
+logging.basicConfig(filename=log_path, level=logging.DEBUG)
+
 current = datetime.today()
+
+logging.info(str(current))
 
 for p in path_list:
     path = HOME_DIR + p
+    logging.debug('before-->')
+    logging.debug(os.getcwd())
     os.chdir(path)
-    git.pull('origin', 'master')
-    print 'Pulled %s' % p
+    logging.debug('after-->')
+    logging.debug(os.getcwd())
+    logging.debug(str(git.pull('origin', 'master')))
+    logging.info('Pulled %s' % p)
 
     git_time_str = re.search('\d{4}-\d{2}-\d{2}', str(git.log('-1', '--date=iso'))).group(0) # Magic line
     last_mod_time = datetime.strptime(git_time_str, '%Y-%m-%d')
     # Check if time diff
     if current.date() - last_mod_time.date() == timedelta(0):
-        print 'Found a new mod path %s, abort update' % p
+        logging.info('Found a new mod path %s, abort update' % p)
         exit(0)
 
 with open(target_path, 'a') as tfile:
@@ -39,12 +50,14 @@ os.chdir(target_path.rsplit('/',1)[0])
 
 
 if args.commit:
-    git.add(target_path)
-    print 'Done with add'
-    git.commit('-m', 'Keep streak on %s' % str(current.date()))
-    print 'Done with commit'
-    git.push('origin', 'master')
-    print 'Done with push'
+    logging.debug(str(git.add(target_path)))
+    logging.info('Done with add')
+    logging.debug(str(git.commit('-m', 'Keep streak on %s' % str(current.date()))))
+    logging.info('Done with commit')
+    logging.debug(str(git.push('origin', 'master')))
+    logging.info('Done with push')
 else:
-    git.checkout(target_path)
-    print 'Aborted and reverted'
+    logging.debug(str(git.checkout(target_path)))
+    logging.info('Aborted and reverted')
+
+logging.info('-'*20)
