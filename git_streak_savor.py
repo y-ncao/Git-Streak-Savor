9430645347d385e63e56f99e2918850b5dec8ee3
Yan Cao <cyandterry@hotmail.com>
2014-08-03 00:21:20 -0400

    Fixed the bug and added script

--- git_streak_savor.py
+++ git_streak_savor.py
@@ -1,67 +1,71 @@
 #!/usr/bin/env python
 
+from sh import git
+from datetime import datetime, timedelta
+from ConfigParser import SafeConfigParser
+
 import argparse
 import re
 import os
 import logging
-from sh import git
-from datetime import datetime, timedelta
 
-# Flag parser for either commit or abort
-parser = argparse.ArgumentParser(description='Save my github streak.')
-parser.add_argument('-c', '--commit', dest='commit', action='store_true',
-                    required=False, default=False, help='Actually commit the change (default: no)')
-args = parser.parse_args()
+config_parser = SafeConfigParser()
+config_parser.read('config')
 
-HOME_DIR = '/home/ycao'
-path_list = ['/Documents/Python_Study',
-             '/Documents/jekyll-blog',]
-target_file = '/Documents/jekyll-blog/_posts/2014-07-12-github-streak-savor.md'
-target_path = HOME_DIR + target_file
+arg_parser = argparse.ArgumentParser(description='Save my github streak.')
+arg_parser.add_argument('-c', '--commit', action='store_true', help='Actually commit the change (default: no)')
+arg_parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity')
+args = arg_parser.parse_args()
 
-log_path = '/home/ycao/Dropbox/src/git_streak_savor.log'
-logging.basicConfig(filename=log_path, level=logging.DEBUG)
+LOG_PATH = config_parser.get('Path', 'log_path')+'git_streak_savor.log'
+
+print LOG_PATH
+if args.verbose:
+    logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG)
+else:
+    logging.basicConfig(filename=LOG_PATH, level=logging.INFO)
+
+main_path    = config_parser.get('Path', 'main_path')
+target_path  = config_parser.get('Path', 'target_path')
+project_list = [config_parser.get('Project', option) for option in config_parser.options('Project')]
 
 current = datetime.today()
 
-logging.info(str(current))
-
+logging.info('='*12+str(current)+'='*12)
 update = True
 
-for p in path_list:
-    path = HOME_DIR + p
-    logging.debug('before-->')
-    logging.debug(os.getcwd())
+for project in project_list:
+    path = main_path + project
+    logging.debug('Before-->'+os.getcwd())
     os.chdir(path)
-    logging.debug('after-->')
-    logging.debug(os.getcwd())
-    logging.debug(str(git.pull('origin', 'master')))
-    logging.info('Pulled %s' % p)
+    logging.debug('After -->'+os.getcwd())
 
-    git_time_str = re.search('\d{4}-\d{2}-\d{2}', str(git.log('-1', '--date=iso'))).group(0) # Magic line
+    git.pull('origin', 'master')
+    logging.info('Pulled %s' % project)
+
+    last_mod = git.log('-1', '--date=iso')
+    logging.debug(str(last_mod))
+    git_time_str = re.search('\d{4}-\d{2}-\d{2}', str(last_mod)).group(0) # Magic line
     last_mod_time = datetime.strptime(git_time_str, '%Y-%m-%d')
 
-    # Check if time diff
+    # Check if last mod == Today
     if current.date() - last_mod_time.date() == timedelta(0):
-        logging.info('Found a new mod path %s, abort update' % p)
+        logging.info('Found a new modified project <%s>, abort update' % project)
         update = False
 
 if update:
-    with open(target_path, 'a') as tfile:
-        tfile.write(str(current.date())+' ')
+    with open(target_path, 'a') as tf:
+        tf.write(str(current.date())+' ')
 
     os.chdir(target_path.rsplit('/',1)[0])
 
-
     if args.commit:
         git.add(target_path)
-        logging.info('Done with add')
-        logging.debug(str(git.commit('-m', 'Keep streak on %s' % str(current.date()))))
-        logging.info('Done with commit')
-        logging.debug(str(git.push('origin', 'master')))
-        logging.info('Done with push')
+        git.commit('-m', 'Keep streak on %s' % str(current.date()))
+        git.push('origin', 'master')
+        logging.info('Pushed to target file at %s' % str(current))
     else:
         git.checkout(target_path)
-        logging.info('Aborted and reverted')
+        logging.info('Abort and revert')
 
-logging.info('-'*20)
+logging.info('='*12+'end'+'='*12)
---
+++ git_streak_savor.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+APPDIR=$HOME/Documents/
+export PYTHONPATH=$APPDIR/Git-Streak-Savor:$APPDIR
+
+cd $APPDIR/Git-Streak-Savor
+./git_streak_savor.py -c
