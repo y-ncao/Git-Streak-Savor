c2f3a19616397a881e536b88379b8d948bca0eac
Yan Cao <cyandterry@hotmail.com>
2014-10-23 15:52:57 -0400

    Fixed the bug that stuck at the git log

--- git_streak_savor.py
+++ git_streak_savor.py
@@ -1,13 +1,54 @@
 #!/usr/bin/env python
 
-from sh import git
+import sh
 from datetime import datetime, timedelta
 from ConfigParser import SafeConfigParser
 
 import argparse
 import re
-import os
 import logging
+
+
+def check_project(main_path, project_list):
+    for project in project_list:
+        path = main_path + project
+        git = sh.git.bake(_cwd=path)
+
+        git('pull', 'origin', 'master')
+        logging.info('Pulled %s' % project)
+
+        last_mod = git('--no-pager', 'log', '-1', '--pretty=format:%ci')
+        logging.info('Last_mod %s' % last_mod)
+        git_time_str = re.search('\d{4}-\d{2}-\d{2}', str(last_mod)).group(0) # Magic line
+        last_mod_time = datetime.strptime(git_time_str, '%Y-%m-%d')
+
+        # Check if last mod == Today
+        if current.date() - last_mod_time.date() == timedelta(0):
+            logging.info('Found a new modified project <%s>, abort update' % project)
+            return False
+    return True
+
+
+def update_project(target_path):
+    with open(target_path, 'a') as tf:
+        tf.write(str(current.date())+' ')
+
+    path = target_path.rsplit('/',1)[0]
+    git = sh.git.bake(_cwd=path)
+
+    if args.commit:
+        git('add', target_path)
+        git('commit', '-m', 'Keep streak on %s' % str(current.date()))
+        git('push', 'origin', 'master')
+        logging.info('Pushed to target file at %s' % str(current))
+    else:
+        git('checkout', target_path)
+        logging.info('Abort and revert')
+
+
+#######################################################
+# MAIN
+#######################################################
 
 config_parser = SafeConfigParser()
 config_parser.read('config')
@@ -17,7 +58,7 @@ arg_parser.add_argument('-c', '--commit', action='store_true', help='Actually co
 arg_parser.add_argument('-v', '--verbose', action='store_true', help='Increase output verbosity')
 args = arg_parser.parse_args()
 
-LOG_PATH = config_parser.get('Path', 'log_path')+'git_streak_savor.log'
+LOG_PATH = config_parser.get('Path', 'log_path') + 'git_streak_savor.log'
 
 if args.verbose:
     logging.basicConfig(filename=LOG_PATH, level=logging.DEBUG)
@@ -30,45 +71,8 @@ project_list = [config_parser.get('Project', option) for option in config_parser
 
 current = datetime.today()
 
-logging.info('='*12+str(current)+'='*12)
-update = True
+logging.info( '='*12 + str(current) + '='*12 )
 
-for project in project_list:
-    path = main_path + project
-    logging.debug('Before-->'+os.getcwd())
-    os.chdir(path)
-    logging.debug('After -->'+os.getcwd())
-
-    git.pull('origin', 'master')
-    logging.info('Pulled %s' % project)
-    logging.debug('broke next')
-    try:
-        last_mod = git.log('-1', '--date=iso')
-    except:
-        logging.debug('has error')
-    logging.debug('Broked')
-    #logging.debug(str(last_mod))
-    git_time_str = re.search('\d{4}-\d{2}-\d{2}', str(last_mod)).group(0) # Magic line
-    last_mod_time = datetime.strptime(git_time_str, '%Y-%m-%d')
-
-    # Check if last mod == Today
-    if current.date() - last_mod_time.date() == timedelta(0):
-        logging.info('Found a new modified project <%s>, abort update' % project)
-        update = False
-
-if update:
-    with open(target_path, 'a') as tf:
-        tf.write(str(current.date())+' ')
-
-    os.chdir(target_path.rsplit('/',1)[0])
-
-    if args.commit:
-        git.add(target_path)
-        git.commit('-m', 'Keep streak on %s' % str(current.date()))
-        git.push('origin', 'master')
-        logging.info('Pushed to target file at %s' % str(current))
-    else:
-        git.checkout(target_path)
-        logging.info('Abort and revert')
-
-logging.info('='*12+'end'+'='*12)
+if check_project(main_path, project_list):
+    update_project(target_path)
+logging.info( '='*22 + ' Done ' + '='*22 + '\n')
